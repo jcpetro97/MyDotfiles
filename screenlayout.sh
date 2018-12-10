22f7c96ed9221a56fb5ea32233259b3bc65a99aa
jxpsys <john.petro@rit.edu>
2018-12-10 15:04:49 -0500

    updated screenlayout.sh for my current monitor layout

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output DisplayPort-1-3 --mode 1920x1080 --pos 3840x0 --rotate normal --output DVI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2-2 --off --output HDMI-2-2 --off --output HDMI-2-3 --off --output DP-2-3 --off --output DP-2-1 --off --output HDMI-2-1 --off --output DVI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-4 --off
